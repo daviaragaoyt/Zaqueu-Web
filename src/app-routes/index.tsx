16c40329adf3624c1d6ce2bd698a484e0adfafd9
Davi <daviaragaodf1@gmail.com>
2025-11-20 20:37:26 -0300

    admin page

--- src/app-routes/index.tsx
+++ src/app-routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages'
 import LoginPage from '../pages/login';
+import AdminPage from '../pages/admin';
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path='/admin' element={<AdminPage/>}/>
     </Routes>
   );
 };
